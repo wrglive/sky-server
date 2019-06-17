7acaf15eec00015baaa429082d7c0f75bbc438d7
live <wangruigang@ricebook.com>
2019-06-18 01:53:24 +0800

    注册中心的实现， 暂未实现 集群。

---
+++ src/main/java/com/marshall/sky/server/SkyServerApplication.java
@@ -0,0 +1,15 @@
+package com.marshall.sky.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SkyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marshall/sky/server/SkyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marshall.sky.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkyServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
